6961ceaf83a41e862e7a103a50b076dde564bdf3
Dakamakat <gazon-ozon@yandex.ru>
2022-06-20 15:50:12 +0300

    add mobile project

---
+++ src/CS.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CS.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/CS.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CS.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
