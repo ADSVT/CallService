c4163ab5c415dab8eab99705ebd283f61221b6f8
kirsanovtaras8@gmail.com <kirsanovtaras8@gmail.com>
2022-06-23 13:18:56 +0300

    CLCS_1_GateWay: done base logic using ocelot, add logging and caching

--- src/CS.ApiGateways/Program.cs
+++ src/CS.ApiGateways/Program.cs
@@ -1,6 +1,49 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using NLog.Web;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
-app.MapGet("/", () => "Hello World!");
+var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+    builder.Host.ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.SetMinimumLevel(LogLevel.Trace);
+    })
+    .UseNLog();
+
+    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+    builder.Configuration.AddJsonFile($"ocelot.{env}.json");
+
+    builder.Services.AddControllers();
+
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddOcelot()
+                    .AddCacheManager(options => options.WithDictionaryHandle());
+
+    var app = builder.Build();
+
+    app.UseHttpsRedirection();
+
+    app.UseOcelot().Wait();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception exception)
+{
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    NLog.LogManager.Shutdown();
+}
